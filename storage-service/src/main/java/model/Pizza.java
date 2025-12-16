03b5665fa335a59cc69dff0c7867345ad45cd307
Rafnes <rafzograf@gmail.com>
2025-12-17 02:20:56 +0300

    storage service: model classes added

---
+++ storage-service/src/main/java/model/Pizza.java
@@ -0,0 +1,4 @@
+package model;
+
+public class Pizza extends Product{
+}
---
+++ storage-service/src/main/java/model/Product.java
@@ -0,0 +1,8 @@
+package model;
+
+public abstract class Product {
+    private Long id;
+    private String name;
+    private Double price;
+    private String description;
+}
